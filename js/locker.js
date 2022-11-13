79305aa275f571e51b98e68149628dd8c03e442a
NazarBezpaliuk <nazardxvx@gmail.com>
2022-11-13 12:52:33 +0200

    1

--- js/locker.js
+++ js/locker.js
@@ -2,8 +2,10 @@ const wraper = document.querySelector("#wraper");
 const input = document.querySelector("#locker_input");
 const button = document.querySelector("#locker_submit");
 
+const PASSWORD = "2819";
+
 const unlock = () => {
-  if (input.value === "2819") {
+  if (input.value === PASSWORD) {
     return wraper.classList.add("wraper_unlock");
   }
   wraper.classList.add("wraper_lock");
